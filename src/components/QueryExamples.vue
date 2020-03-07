882d84c63899734e8c7f931cf93f4a0699d78324
Waidhoferj <jwaidhof@calpoly.edu>
2020-03-07 15:29:24 -0800

    added query examples

---
+++ src/components/QueryExamples.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="query-examples">
+    <h2>Stuff Nimbus Knows</h2>
+    <ul class="example-list">
+      <li
+        v-for="example in examples"
+        @click="selectExample"
+        class="example"
+        :key="example"
+      >{{example}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import examples from "@/modules/query-examples.js";
+export default {
+  data() {
+    return {
+      examples: examples
+    };
+  },
+  methods: {
+    selectExample(e) {
+      this.$emit("select", e.target.innerText);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.query-examples {
+  z-index: 7;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: var(--base);
+  color: white;
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+
+  h2 {
+    text-align: center;
+  }
+  .example-list {
+    list-style: none;
+    max-width: 400px;
+    margin: auto;
+    .example {
+      cursor: pointer;
+      padding: 20px 0;
+    }
+  }
+}
+</style>
---
+++ src/modules/query-examples.js
@@ -0,0 +1,7 @@
+export default [
+  "What is Professor Lupo's email?",
+  "Where is Dr. Koshmood's office?",
+  "What are the prerequisites for CPE 202?",
+  "What is the course number for Systems Programming?",
+  "What is CSAI?"
+];
