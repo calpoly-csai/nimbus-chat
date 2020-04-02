199c47cc05b82462cc580183af37b77489e2db64
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2020-04-02 16:45:18 -0700

    Add query examples

--- src/modules/query-examples.js
+++ src/modules/query-examples.js
@@ -3,5 +3,16 @@ export default [
   "Where is Dr. Koshmood's office?",
   "What are the prerequisites for CPE 202?",
   "What is the course number for Systems Programming?",
-  "What is CSAI?"
+  "What is CSAI?",
+  "How many units is CSC 348?",
+  "When is Artificial Intelligence typically offered?",
+  "What is Programming Languages about?",
+  "What are Professor Kurfess's research areas?",
+  "What is Dr. Chris Lupo's homepage?",
+  "What is the box number of the CSAI club?",
+  "How do I learn more about the CSAI club?",
+  "Is there a contact for CSAI?",
+  "What college is affiliated with the CSAI club?",
+  "Who is the club adviser for CSAI?",
+  "Which professors have taught CSC 480?"
 ];
